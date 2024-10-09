ad294e9ac55d7d351af9c70e738d5883be5d3fc0
Gustavo-otacilio <gustavo.otacilio@grupotecnoset.com.br>
2024-10-09 11:50:03 -0300

    alterando quantidade de números a serem escolhidos para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let numerosSorteados = [];
-let limite = 10;
+let limite = 100;
 let numeroSecreto = numeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, text) {
 
 function exibirMsgInicial() {
   exibirTextoNaTela("h1", "Escolha um número");
-  exibirTextoNaTela("p", "Escolha um número de 1 - 10");
+  exibirTextoNaTela("p", "Escolha um número de 1 - 100");
 }
 
 exibirMsgInicial();
